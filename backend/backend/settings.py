d7f4f58cf4b2d3a4113a651a0d3f41b05fc4871b
fix: ELIAS <115664689+Elias-Front-end@users.noreply.github.com>
2025-11-02 21:19:41 -0300

    fix: Desabilitar logging em arquivo para resolver problema de permissões

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -298,12 +298,12 @@ LOGGING = {
         },
     },
     'handlers': {
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': BASE_DIR / 'logs' / 'django.log',
-            'formatter': 'verbose',
-        },
+        # 'file': {
+        #     'level': 'INFO',
+        #     'class': 'logging.FileHandler',
+        #     'filename': BASE_DIR / 'logs' / 'django.log',
+        #     'formatter': 'verbose',
+        # },
         'console': {
             'level': 'DEBUG' if DEBUG else 'INFO',
             'class': 'logging.StreamHandler',
@@ -311,12 +311,12 @@ LOGGING = {
         },
     },
     'root': {
-        'handlers': ['console', 'file'],
+        'handlers': ['console'],
         'level': 'INFO',
     },
     'loggers': {
         'django': {
-            'handlers': ['console', 'file'],
+            'handlers': ['console'],
             'level': 'INFO',
             'propagate': False,
         },
